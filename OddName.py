598ba5e5263494dc17d5c2270a891fd9bb81808f
Ben White <benjamin.white1@my.jcu.edu.au>
2016-08-15 09:41:00 +1000

    add a name printing program

--- OddName.py
+++ OddName.py
@@ -2,3 +2,12 @@
 
 Ben White
 """
+
+userName = input("Please enter your name: ")
+
+while len(userName) == 0:
+
+    userName = input("Please enter your name")
+for i in range(0, len(userName), 2):
+    print(userName[i], end='')
+
